d4537f9e460e4a8a7d64da6a44dd4ae61efdcf7c
trpa-mason <mbindl@trpa.org>
2025-04-23 23:13:22 -0700

    update maps and fuels chart

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from arcpy.ia import *
 from arcpy.ddd import *
 import pandas as pd
 from pathlib import Path
-
+import plotly.express as px
 # functions
 # function to classify raw raster into desired condition categories based on 90th and 10th percentile value comaprison 
 def raster_categorize(input_raster, upper_raster, lower_raster, output_raster_name):
@@ -112,3 +112,288 @@ def raster_to_df(raster_path, band=1):
     df["Rows"] = rows
     df["Cols"] = cols
     return df
+
+
+## From CR DASHBOARD ##
+
+# Gets spatially enabled dataframe from TRPA server
+def get_fs_data_spatial(service_url):
+    feature_layer = FeatureLayer(service_url)
+    query_result = feature_layer.query().sdf
+    return query_result
+
+# Gets data from the TRPA server
+def get_fs_data(service_url):
+    feature_layer = FeatureLayer(service_url)
+    query_result = feature_layer.query()
+    # Convert the query result to a list of dictionaries
+    feature_list = query_result.features
+    # Create a pandas DataFrame from the list of dictionaries
+    all_data = pd.DataFrame([feature.attributes for feature in feature_list])
+    # return data frame
+    return all_data
+
+# Stacked Percent Bar chart
+def stackedbar(
+    df,
+    path_html,
+    div_id,
+    x,
+    y,
+    color,
+    color_sequence,
+    orders,
+    y_title,
+    x_title,
+    custom_data,
+    hovertemplate,
+    hovermode,
+    format,
+    name=None,
+    additional_formatting=None,
+    orientation=None,
+    facet=None,
+    facet_row=None,
+):
+    config = {"displayModeBar": False}
+    fig = px.bar(
+        df,
+        x=x,
+        y=y,
+        color=color,
+        barmode="stack",
+        facet_col=facet,
+        facet_row=facet_row,
+        color_discrete_sequence=color_sequence,
+        category_orders=orders,
+        orientation=orientation,
+        custom_data=custom_data,
+    )
+
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+
+    fig.update_layout(
+        yaxis=dict(tickformat=format, hoverformat=format, title=y_title),
+        xaxis=dict(title=x_title),
+        hovermode=hovermode,
+        template="plotly_white",
+        dragmode=False,
+        legend_title=None,
+    )
+    fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True, tickformat=format))
+    # fig.for_each_yaxis(lambda yaxis: yaxis.update(tickfont = dict(color = 'rgba(0,0,0,0)')), secondary_y=True)
+    fig.update_yaxes(
+        col=2, row=1, showticklabels=False, tickfont=dict(color="rgba(0,0,0,0)"), title=None
+    )
+    fig.update_yaxes(
+        col=3, row=1, showticklabels=False, tickfont=dict(color="rgba(0,0,0,0)"), title=None
+    )
+    fig.update_xaxes(tickformat=".0f")
+    fig.update_traces(hovertemplate=hovertemplate)
+    fig.update_layout(additional_formatting)
+
+    fig.write_html(
+        config=config,
+        file=path_html,
+        include_plotlyjs="directory",
+        div_id=div_id,
+    )
+
+# natural_systems.py
+def get_data_forest_fuel():
+    eipForestTreatments = "https://www.laketahoeinfo.org/WebServices/GetReportedEIPIndicatorProjectAccomplishments/JSON/e17aeb86-85e3-4260-83fd-a2b32501c476/19"
+    data = pd.read_json(eipForestTreatments)
+    df = data[data["PMSubcategoryName1"] == "Treatment Zone"]
+    df = df.rename(
+        columns={
+            "IndicatorProjectYear": "Year",
+            "PMSubcategoryOption1": "Treatment Zone",
+            "IndicatorProjectValue": "Acres",
+        }
+    )
+    # change value Community Defense Zone to Defense Zone for consistency
+    df["Treatment Zone"] = df["Treatment Zone"].replace("Community Defense Zone", "Defense Zone")
+    df["Year"] = df["Year"].astype(str)
+    df = df.groupby(["Year", "Treatment Zone"]).agg({"Acres": "sum"}).reset_index()
+    return df
+
+
+def plot_forest_fuel(df):
+    stackedbar(
+        df,
+        path_html="DataVisualizations\FuelTreatment_Chart.html",
+        div_id="ForestFuelsTreatement_Chart",
+        x="Year",
+        y="Acres",
+        facet=None,
+        color="Treatment Zone",
+        # color_sequence=["#208385", "#FC9A62", "#F9C63E", "#632E5A", "#A48352", "#BCEDB8"],
+        color_sequence=["#ABCD66", "#E69800", "#A87000", "#F5CA7A"],
+        orders={
+            "Year": [
+                "2007",
+                "2008",
+                "2009",
+                "2010",
+                "2011",
+                "2012",
+                "2013",
+                "2014",
+                "2015",
+                "2016",
+                "2017",
+                "2018",
+                "2019",
+                "2020",
+                "2021",
+                "2022",
+                "2023",
+            ]
+        },
+        y_title="Acres Treated",
+        x_title="Year",
+        custom_data=["Treatment Zone"],
+        hovertemplate="<br>".join(
+            ["<b>%{y:,.0f} acres</b> of forest health treatment", "in the <i>%{customdata[0]}</i>"]
+        )
+        + "<extra></extra>",
+        hovermode="x unified",
+        orientation=None,
+        format=",.0f",
+        additional_formatting=dict(
+            # title = "Forest Health Treatment",
+            legend=dict(
+                title= "Forest Health Treatment Zone",
+                orientation="h",
+                entrywidth=85,
+                yanchor="bottom",
+                y=1.05,
+                xanchor="right",
+                x=0.95,
+            )
+        ),
+    )
+
+
+def get_old_growth_forest():
+    data = get_fs_data(
+        "https://maps.trpa.org/server/rest/services/Vegetation_Late_Seral/FeatureServer/0"
+    )
+    # df = data.groupby(["SeralStage","SpatialVar"]).agg({"Acres": "sum"}).reset_index()
+    df = data[["SeralStage", "SpatialVar", "TRPA_VegType", "Acres"]]
+    return df
+
+
+def plot_old_growth_forest(df):
+    seral = df.groupby("SeralStage").agg({"Acres": "sum"}).reset_index()
+    stackedbar(
+        seral,
+        path_html="html/2.1.b_OldGrowthForest_SeralStage.html",
+        div_id="2.1.b_OldGrowthForest_SeralStage",
+        x="SeralStage",
+        y="Acres",
+        facet=None,
+        color=None,
+        color_sequence=["#208385"],
+        orders=None,
+        y_title="Acres",
+        x_title="Seral Stage",
+        custom_data=["SeralStage"],
+        hovertemplate="<br>".join(["<b>%{y:,.0f}</b> acres of", "<i>%{customdata[0]}</i> forest"])
+        + "<extra></extra>",
+        hovermode="x unified",
+        orientation=None,
+        format=",.0f",
+    )
+    structure = df.groupby("SpatialVar").agg({"Acres": "sum"}).reset_index()
+    stackedbar(
+        structure,
+        path_html="html/2.1.b_OldGrowthForest_Structure.html",
+        div_id="2.1.b_OldGrowthForest_Structure",
+        x="SpatialVar",
+        y="Acres",
+        facet=None,
+        color=None,
+        color_sequence=["#208385"],
+        orders=None,
+        y_title="Acres",
+        x_title="Structure",
+        custom_data=["SpatialVar"],
+        hovertemplate="<br>".join(
+            ["<b>%{y:,.0f}</b> acres of", "<i>%{customdata[0]}</i> old growth forest"]
+        )
+        + "<extra></extra>",
+        hovermode="x unified",
+        orientation=None,
+        format=",.0f",
+    )
+    species = df.groupby("TRPA_VegType").agg({"Acres": "sum"}).reset_index()
+    stackedbar(
+        species,
+        path_html="html/2.1.b_OldGrowthForest_Species.html",
+        div_id="2.1.b_OldGrowthForest_Species",
+        x="TRPA_VegType",
+        y="Acres",
+        facet=None,
+        color=None,
+        color_sequence=["#208385"],
+        orders=None,
+        y_title="Acres",
+        x_title="Vegetation Type",
+        custom_data=["TRPA_VegType"],
+        hovertemplate="<br>".join(
+            ["<b>%{y:,.0f}</b> acres of", "<i>%{customdata[0]}</i> old growth forest"]
+        )
+        + "<extra></extra>",
+        hovermode="x unified",
+        orientation=None,
+        format=",.0f",
+    )
+
+def get_probability_of_high_severity_fire():
+    highseverity = get_fs_data_spatial(
+        "https://maps.trpa.org/server/rest/services/LTinfo_Climate_Resilience_Dashboard/MapServer/129"
+    )
+    df = highseverity.groupby(["Name", "gridcode"])["Acres"].sum().reset_index()
+    df["Probability"] = np.where(
+        df["gridcode"] == 1, "High Severity Fire", "Low to Moderate Severity Fire"
+    )
+
+    # standardize values to "Wilderness"
+    df.loc[
+        df["Name"].isin(
+            ["Desolation Wilderness", "Mt. Rose Wilderness", "Granite Chief Wilderness"]
+        )
+    ] = "Wilderness"
+
+    total = df.groupby("Name")["Acres"].sum().reset_index()
+
+    df = df.merge(total, on="Name")
+    df["Share"] = df["Acres_x"] / df["Acres_y"]
+    df = df.rename(
+        columns={"Name": "Forest Management Zone", "Acres_x": "Acres", "Acres_y": "Total"}
+    )
+    return df
+
+def plot_probability_of_high_severity_fire(df):
+    stackedbar(
+        df,
+        path_html="html/2.1.c_Probability_of_High_Severity_Fire.html",
+        div_id="2.1.c_Probability_of_High_Severity_Fire",
+        x="Forest Management Zone",
+        y="Share",
+        facet=None,
+        color="Probability",
+        color_sequence=["#208385", "#FCB42C"],
+        orders=None,
+        y_title="",
+        x_title="Forest Management Zone",
+        custom_data=["Probability"],
+        hovertemplate="<br>".join(
+            ["<b>%{y:.0%}</b> of the forested area is", "likely to burn as <i>%{customdata[0]}</i>"]
+        )
+        + "<extra></extra>",
+        hovermode="x unified",
+        orientation=None,
+        format=".0%",
+    )
